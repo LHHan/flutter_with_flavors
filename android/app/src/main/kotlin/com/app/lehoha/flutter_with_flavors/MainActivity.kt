586641f8d3f5d9f99ab348366ce45058dba8c12d
Lehoha <hoanghan.le.95@gmail.com>
2023-02-01 17:13:00 +0700

    Config flavors

--- android/app/src/main/kotlin/com/app/lehoha/flutter_with_flavors/MainActivity.kt
+++ android/app/src/main/kotlin/com/app/lehoha/flutter_with_flavors/MainActivity.kt
@@ -1,6 +1,17 @@
 package com.app.lehoha.flutter_with_flavors
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import androidx.annotation.NonNull;
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "flavor").setMethodCallHandler {
+            call, result -> result.success(BuildConfig.FLAVOR)
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+Future<void> main() async {
+  /// Start services later
+  WidgetsFlutterBinding.ensureInitialized();
+
+  /// Load environment and set app config
+  await _loadEnvironment();
+
   runApp(const MyApp());
 }
 
@@ -63,5 +70,63 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+/// Environments type enum declare here
+enum EnvType { dev, prod }
+
+/// Environment declare here
+class Env {
+  Env._({
+    required this.envType,
+    required this.apiBaseUrl,
+  });
+
+  final EnvType envType;
+  final String apiBaseUrl;
+
+  /// Dev mode
+  factory Env.dev() {
+    return Env._(
+      envType: EnvType.dev,
+      apiBaseUrl: 'https://devapi.lehoha.app.com',
+    );
+  }
+
+  /// Prod mode
+  factory Env.prod() {
+    return Env._(
+      envType: EnvType.prod,
+      apiBaseUrl: 'https://prodapi.lehoha.app.com',
+    );
+  }
+}
+
+/// Config env
+class AppConfig {
+  factory AppConfig({required Env env}) {
+    I.env = env;
+    return I;
+  }
+
+  AppConfig._private();
+
+  static final AppConfig I = AppConfig._private();
+
+  Env env = Env.dev();
+}
+
+Future<void> _loadEnvironment() async {
+  /// Get Flavor value from native via method channel
+  String? flavor =
+      await const MethodChannel('flavor').invokeMethod<String>('getFlavor');
+
+  if (flavor == EnvType.dev.name) {
+    AppConfig(env: Env.dev());
+  } else if (flavor == EnvType.prod.name) {
+    AppConfig(env: Env.prod());
+  } else {
+    throw Exception("Unknown flavor: $flavor");
   }
 }
